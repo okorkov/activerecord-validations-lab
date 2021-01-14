3d04f7d62f51afcc2ebc00e7d1afa6757b15b272
okorkov <alex.okorkov@gmail.com>
2021-01-13 18:48:47 -0800

    commit tree 13b5deba61f583ee5e850367b7cea803a968c5b0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 10}
+  validates :summary, length: {maximum: 10}
+  validates :category, acceptance: { accept: ['TRUE', 'accepted', 'Fiction'] }
+  validates :title, acceptance: { accept: ["You Won't Believe These True Facts"] }
 end
